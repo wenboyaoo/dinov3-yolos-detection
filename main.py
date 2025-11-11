b940727f11cac3f09171d0fade930989e9ec69a2
wenboyaoo <wen_boyao@163.com>
2025-11-11 23:23:25 +0800

    debug

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def get_args_parser():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
-    parser.add_argument('--config_path',default='config.yaml', help='path to .yaml configuration file')
+    parser.add_argument('--config_path',default=None, help='path to .yaml configuration file')
     return parser
 
 
