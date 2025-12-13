0da34a25bd79849c155e73aaff70c22f6c04c1cb
wenboyaoo <wen_boyao@163.com>
2025-12-13 14:29:36 +0800

    update

--- models/detector.py
+++ models/detector.py
@@ -298,7 +298,7 @@ def build(args):
     #     weight_dict.update(aux_weight_dict)
 
     losses = ['labels', 'boxes', 'cardinality']
-    criterion = SetCriterion(num_classes, matcher=matcher, weight_dict=weight_dict,
+    criterion = SetCriterion(args.num_classes+1, matcher=matcher, weight_dict=weight_dict,
                              eos_coef=args.eos_coef, losses=losses)
     criterion.to(device)
     postprocessors = {'bbox': PostProcess()}
